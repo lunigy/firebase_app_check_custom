73a911a7778ae6a966d3209b5ece966909d4deb8
“Bill <igottalentco@gmail.com>
2025-04-25 13:23:44 +1000

    Remove unused import

--- lib/src/firebase_app_check.dart
+++ lib/src/firebase_app_check.dart
@@ -3,8 +3,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io' show Platform;
-
 part of '../firebase_app_check_custom.dart';
 
 class FirebaseAppCheck extends FirebasePluginPlatform {
